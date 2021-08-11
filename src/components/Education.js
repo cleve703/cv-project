1311ef42e2cd87f776d8fdc42ed80382777cd493
Jay <cleve703@gmail.com>
2021-08-10 23:29:41 -0400

    Add displaying school data from array.

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,35 +2,50 @@ import React from "react";
 
 class Education extends React.Component {
   render() {
-    const { schoolData, handleChangeEducation, addSchool } = this.props;
+    const { schoolData, handleChangeEducation, addSchool, schools } = this.props;
     return (
-      <fieldset>
-        <input
-          onChange={ e => handleChangeEducation(e)}
-          name="schoolName"
-          placeholder="School Name"
-          value={schoolData.schoolName}
-        />
-        <input
-          onChange={ e => handleChangeEducation(e)}
-          name="schoolLocation"
-          placeholder="School Location"
-          value={schoolData.schoolLocation}
-        />
-        <input
-          onChange={ e => handleChangeEducation(e)}
-          name="degreeType"
-          placeholder="Degree Type"
-          value={schoolData.degreeType}
-        />
-        <input
-          onChange={ e => handleChangeEducation(e)}
-          name="datesAttended"
-          placeholder="Dates Attended"
-          value={schoolData.datesAttended}
-        />
-        <button type="button" onClick={addSchool}>Add a school</button>
-      </fieldset>
+      <div>
+        <ul class="naked-list">
+          {schools.map((school) => {
+            return  ( <li key={school.id}>
+                        <ul class="naked-list">
+                          <li>{school.schoolName}</li>
+                          <li>{school.schoolLocation}</li>
+                          <li>{school.degreeType}</li>
+                          <li>{school.datesAttended}</li>
+                        </ul>
+                      </li>
+                    )
+          })}
+        </ul>
+        <fieldset>
+          <input
+            onChange={ e => handleChangeEducation(e)}
+            name="schoolName"
+            placeholder="School Name"
+            value={schoolData.schoolName}
+          />
+          <input
+            onChange={ e => handleChangeEducation(e)}
+            name="schoolLocation"
+            placeholder="School Location"
+            value={schoolData.schoolLocation}
+          />
+          <input
+            onChange={ e => handleChangeEducation(e)}
+            name="degreeType"
+            placeholder="Degree Type"
+            value={schoolData.degreeType}
+          />
+          <input
+            onChange={ e => handleChangeEducation(e)}
+            name="datesAttended"
+            placeholder="Dates Attended"
+            value={schoolData.datesAttended}
+          />
+          <button type="button" onClick={addSchool}>Add a school</button>
+        </fieldset>
+      </div>
     )
   }
 }
